4414cbaf611d25dd690c5a112f18881630e3f6a6
heisen201 <primetips56@gmail.com>
2025-01-30 20:24:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://heisenn:nedim12@cluster0.wdn3p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "/play | Dev Heisen", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
